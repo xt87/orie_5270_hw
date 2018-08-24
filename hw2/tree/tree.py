4f08156307f9f7885f984cb5c60446e2b7a38d1d
xt87 <42581437+xt87@users.noreply.github.com>
2018-08-23 22:28:26 -0400

    Update tree.py

--- hw2/tree/tree.py
+++ hw2/tree/tree.py
@@ -1,14 +1,17 @@
 class Tree(object):
     def __init__(self, root):
+        """
+        This is to initialize the root
+        :param root : the root of the tree, which is in the format of Node
+        """
         self.root = root
-
-    def get_value_root(self):
-        if self.root is not None:
-            return self.root.value
-        else:
-            return None
         
-    def get_depth(self,root):
+    def get_level(self,root):
+        """
+        This is to get the number of levels, or the depth, of the tree
+        :param root : the root of the tree as a node, which is the upmost node of the tree
+        :return : the number of levels of your tree
+        """
         if root is None:
             return 0
         elif isinstance(root,Node) and root.left is None and root.right is None:
@@ -19,6 +22,14 @@ class Tree(object):
             return 1
     
     def insert(self,root,arr,row,col):
+        """
+        this can insert nodes to the desiered position of your tree
+        :param root : the upmost node of your tree,in the format of Node
+        :param arr : an array that contains every position and stucture of your tree
+        :param row : an integer which specifies the the row position of the node
+        :param col : an integer which specifies the column position of the node
+        :return : the array with the node inserted
+        """
         if isinstance(root, Node):
             arr[-row-1][col] = root.value
             if root.left:
@@ -30,7 +41,11 @@ class Tree(object):
         return arr
     
     def printTree(self):
-        depth = self.get_depth(self.root)
+        """
+        this can print the tree
+        :return : your tree in the format of an array
+        """
+        depth = self.get_level(self.root)
         n = 2**depth-1
         arr = [['|'for i in range(n)]for j in range(depth)]
         arr = self.insert(self.root,arr,depth-1,2**(depth-1)-1)
@@ -39,6 +54,13 @@ class Tree(object):
 class Node(object):
 
     def __init__(self, value, left, right):
+        """
+        this can specify how the node looks like
+        :param value : the value of the node
+        :param left : the value of the left branch of the node
+        :param right : the value of the right branch of the node
+        :return : the node with its value, left and right branches specified
+        """
         self.value = value
         self.left = left
         self.right = right
