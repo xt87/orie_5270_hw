37c39e12ae4bdc116ddbfae164f079f484f4db68
xt87 <42581437+xt87@users.noreply.github.com>
2018-08-27 21:03:50 -0400

    Update negative_cycle.py

--- HW3/negtive_cycle/negative_cycle.py
+++ HW3/negtive_cycle/negative_cycle.py
@@ -3,20 +3,20 @@ def trans(lines):
     for i in range(len(lines)):
         if i%2 == 1:
             if lines[i]=='\n':
-                trans.append((lines[i-1][0],lines[i],lines[i]))  
+                trans.append((float(lines[i-1][0]),None,None)) 
             else:
                 for j in range(len(lines[i])):
                     if lines[i][j]=='(':
                         if lines[i][j+3]!='-':
-                            trans.append((lines[i-1][0],lines[i][j+1],lines[i][j+3]))
+                            trans.append((float(lines[i-1][0]),float(lines[i][j+1]),float(lines[i][j+3])))
                         else:
-                            trans.append((lines[i-1][0],lines[i][j+1],lines[i][j+3]+lines[i][j+4]))
+                            trans.append((float(lines[i-1][0]),float(lines[i][j+1]),float(lines[i][j+3]+lines[i][j+4])))
             
             
     return trans
     
 def find_negative_cycles(name_txt_file):
-    files = open('name_txt_file','r')
+    files = open(name_txt_file,'r')
     lines = files.readlines()
     tran = trans(lines)
     edges = defaultdict(list)
@@ -25,36 +25,34 @@ def find_negative_cycles(name_txt_file):
         edges[each[0]].append(each[1])
         weights[(each[0],each[1])] = each[2]
     
-    for node in edges.keys():
+    for source in edges.keys():
         d = {}
         p = {}
-        for node in edges.keys():
-            d[node] = float('Inf')
-            p[node] = None
+        for other_node in edges.keys():
+            d[other_node] = float('Inf')
+            p[other_node] = None
+        d[source]=0
     
-    i=0
-    while i<len(edges.keys())-1:
+        for i in range(len(edges.keys())-1):
+            for node in edges.keys():
+                if edges.get(node) != [None]:
+                    for neighbour in edges.get(node):
+                    #print(neighbour,'b')
+                        if d[neighbour] > d[node] + int(weights.get((node,neighbour))):
+                            d[neighbour] = d[node] + int(weights.get((node,neighbour)))
+                            p[neighbour] = node
+  
         for node in edges.keys():
-            d[node]=0
-            if edges.get(node) != ['\n']:
+            if edges.get(node) != [None]:
                 for neighbour in edges.get(node):
                     if d[neighbour] > d[node] + int(weights.get((node,neighbour))):
-                        d[neighbour] = d[node] + int(weights.get((node,neighbour)))
-                        p[neighbour] = node
-            else:
-                pass
-        i = i+1
-    
-    negative_cycle=[]
-    for node in d.keys():
-        if d[node]<0:
-            print('Found a negative cycle.')
-            start = node
-            negative_cycle=[start]
-            backward = p[start]
-            while backward != start and backward:
-                negative_cycle.append(backward)
-                backward = p[backward]
-            return negative_cycle
-        else:
-            return None
+                        #print('Found a negative cycle')
+                        negative_cycle=[node]
+                        back = p[node]
+                        while node != back and back:
+                            negative_cycle.append(back)
+                            back = p[back]
+                        negative_cycle.append(back)
+                        negative_cycle[::-1]
+                        return negative_cycle
+    return None
